979b291525a9c86a399f3a2375baf37978a0dd6b
Kye <kye.ottaway@student.adelaide.edu.au>
2025-08-22 23:29:43 +0930

    who knows

--- even.c
+++ even.c
@@ -4,22 +4,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// Handle SIGINT (Ctrl+C)
-void sig_int(int int_signal) {
-  printf("Yeah!\n");
-  fflush(stdout);
+// Handler for SIGINT (Ctrl+C)
+void sig_int(int sig) {
+  write(STDOUT_FILENO, "Yeah!\n", 6);  // async-signal-safe
 }
 
-// Handle SIGHUP (hangup)
-void sig_hup(int hup_signal) {
-  printf("Ouch!\n");
-  fflush(stdout);
+// Handler for SIGHUP
+void sig_hup(int sig) {
+  write(STDOUT_FILENO, "Ouch!\n", 6);  // async-signal-safe
 }
 
 int main(int argc, char *argv[]) {
-  // Install signal handlers
-  signal(SIGINT, sig_int);
-  signal(SIGHUP, sig_hup);
+  struct sigaction sa_int, sa_hup;
+
+  // Set up SIGINT handler
+  sa_int.sa_handler = sig_int;
+  sigemptyset(&sa_int.sa_mask);
+  sa_int.sa_flags =
+      SA_RESTART;  // restart syscalls like sleep/printf after signal
+  sigaction(SIGINT, &sa_int, NULL);
+
+  // Set up SIGHUP handler
+  sa_hup.sa_handler = sig_hup;
+  sigemptyset(&sa_hup.sa_mask);
+  sa_hup.sa_flags = SA_RESTART;
+  sigaction(SIGHUP, &sa_hup, NULL);
 
   // Check command line argument
   if (argc != 2) {
@@ -27,7 +36,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  int number = atoi(argv[1]);  // convert command line argument to int
+  int number = atoi(argv[1]);
   if (number < 0) {
     fprintf(stderr, "Please enter a positive number\n");
     return 1;
