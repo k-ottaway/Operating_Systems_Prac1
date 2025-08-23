643f692996693909167e927f7d169e37d158d0f1
Kye <kye.ottaway@student.adelaide.edu.au>
2025-08-23 17:13:39 +0930

    blaks help

--- even.c
+++ even.c
@@ -3,48 +3,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void handle_signal(int signal) {
-  if (signal == SIGHUP) {
-    printf("Ouch!");
-    fflush(stdout);
-  } else if (signal == SIGINT) {
-    printf("Yeah!");
-    fflush(stdout);
-  }
+void handle_signals(int signal) {
+  // and prints a message accordingly
+  if (signal == SIGHUP)
+    printf("Ouch!\n");
+  else if (signal == SIGINT)
+    printf("Yeah!\n");
+  fflush(stdout);  // immediately prints output
 }
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s n\n", argv[0]);
-    return 1;
-  }
+int main(int arg_command, char *arg_vector[]) {
+  int n = atoi(arg_vector[1]);  // convert the string to an integer
 
-  int n = atoi(argv[1]);
-  if (n < 0) {
-    fprintf(stderr, "n must be non-negative\n");
-    return 1;
-  }
-
-  // Use sigaction (preferred over signal)
-  struct sigaction sa;
-  sa.sa_handler = handle_signal;
-  sa.sa_flags = 0;
-  sigemptyset(&sa.sa_mask);
-
-  if (sigaction(SIGHUP, &sa, NULL) == -1) {
-    perror("sigaction SIGHUP");
-    return 1;
-  }
-  if (sigaction(SIGINT, &sa, NULL) == -1) {
-    perror("sigaction SIGINT");
-    return 1;
-  }
+  signal(SIGHUP, handle_signals);  // call signals handler
+  signal(SIGINT, handle_signals);
 
   for (int i = 0; i < n; i++) {
-    printf("%d\n", 2 * i);
+    printf(" %d\n", i * 2);  // print the first n even numbers
     fflush(stdout);
     sleep(5);
   }
-
   return 0;
 }
