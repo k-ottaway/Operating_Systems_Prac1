63e452c9317a86cd73044bf81aba0c6696eb9202
Kye <kye.ottaway@student.adelaide.edu.au>
2025-08-23 15:54:48 +0930

    does it work

--- even.c
+++ even.c
@@ -1,53 +1,33 @@
-// even.c
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-// Handler for SIGINT (Ctrl+C)
-void sig_int(int sig) {
-  write(STDOUT_FILENO, "Yeah!\n", 6);  // async-signal-safe
-}
-
-// Handler for SIGHUP
-void sig_hup(int sig) {
-  write(STDOUT_FILENO, "Ouch!\n", 6);  // async-signal-safe
+void handle_signal(int signal) {
+  if (signal == SIGHUP) {
+    printf("Ouch!");
+    fflush(stdout);
+  } else if (signal == SIGINT) {
+    printf("Yeah!");
+    fflush(stdout);
+  }
 }
 
 int main(int argc, char *argv[]) {
-  struct sigaction sa_int, sa_hup;
-
-  // Set up SIGINT handler
-  sa_int.sa_handler = sig_int;
-  sigemptyset(&sa_int.sa_mask);
-  sa_int.sa_flags =
-      SA_RESTART;  // restart syscalls like sleep/printf after signal
-  sigaction(SIGINT, &sa_int, NULL);
-
-  // Set up SIGHUP handler
-  sa_hup.sa_handler = sig_hup;
-  sigemptyset(&sa_hup.sa_mask);
-  sa_hup.sa_flags = SA_RESTART;
-  sigaction(SIGHUP, &sa_hup, NULL);
-
-  // Check command line argument
   if (argc != 2) {
-    fprintf(stderr, "Usage: %s <positive number>\n", argv[0]);
+    fprintf(stderr, "Usage: %s n\n", argv[0]);
     return 1;
   }
 
-  int number = atoi(argv[1]);
-  if (number < 0) {
-    fprintf(stderr, "Please enter a positive number\n");
-    return 1;
-  }
+  int n = atoi(argv[1]);
+  signal(SIGINT, handle_signal);
+  signal(SIGHUP, handle_signal);
 
-  // Print the first n even numbers
-  for (int i = 0; i < 2 * number; i += 2) {
+  // prints the first n even numbers starting from 0
+  for (int i = 0; i < 2 * n; i += 2) {
     printf("%d ", i);
     fflush(stdout);
-    sleep(5);  // delay to allow time for signals
+    sleep(5);
   }
-  printf("\n");
   return 0;
 }
